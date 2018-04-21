0c2247072f0e58eccfe50ea0f67aa5daaaa464ba
Charles Adeeko <cadeeko@googlemail.com>
2018-04-21 19:33:12 +0100

    test(string_challenages): completed reverse a string challenge.

---
+++ HackerRank/src/string_Challenages/java_String_Reverse.java
@@ -0,0 +1,52 @@
+package string_Challenages;
+
+import java.io.*;
+import java.util.*;
+
+public class java_String_Reverse {
+	
+public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        
+      /* Reversing String using converting String into Bytes: 
+       * 
+        // can't reverse a string in java because it's immutable so
+        // convert string into bytes by get bytes method
+        byte[] reverseStringByteArray = A.getBytes();
+        
+        byte[] result = new byte [reverseStringByteArray.length];
+        // store results in reverse order into the the result byte[]
+        for (int i = 0; i < reverseStringByteArray.length; i++) {
+        	 result[i] = reverseStringByteArray[reverseStringByteArray.length-i-1]; 
+        }
+        
+        //Covert results byte array back to string.
+        String finalReverseString = new String(result);
+        */
+        
+        StringBuilder finalReverseString = new StringBuilder();
+        
+        // append a string into StringBuilder finalReverseString
+        finalReverseString.append(A);
+        
+        // reverse string finalReverseString
+        finalReverseString = finalReverseString.reverse();
+        
+        String cake = finalReverseString.toString();
+        
+        
+        
+        
+        //using the string.equals methods to compare content of both objects A and finalReverseString
+        // using == will only checks if both string objects use the same memory location
+        if (A.equals(cake)) {
+        	System.out.println("Yes");
+        	
+        } else {
+        	System.out.println("No");
+        }
+        
+    }
+}
