e6f74dcb9b76795023ece73cb14db5be80b87533
Charles Adeeko <cadeeko@googlemail.com>
2018-04-28 22:36:04 +0100

    test(string_Challenages): add WIP string tokens challenage

--- HackerRank/src/string_Challenages/Java_Anagrams.java
+++ HackerRank/src/string_Challenages/Java_Anagrams.java
@@ -3,44 +3,47 @@ package string_Challenages;
 import java.io.*;
 import java.util.*;
 
-
 public class Java_Anagrams {
-	
+
+
 	static boolean isAnagram(String a, String b) {
-         // Complete the function
-		//make both strings lower case, so they are case insensitive when comparing
+		// Complete the function
+		// make both strings lower case, so they are case insensitive when comparing
 		a = a.toLowerCase();
 		b = b.toLowerCase();
-		
-		//sort string into alphabetical order using string function for sorting created
+
+		// sort string into alphabetical order using string function for sorting created
 		a = alphabSortString(a);
 		b = alphabSortString(b);
 
-		//then compare for a equal match or not on both strings letters and sequence and return boolean value true or false
+		// then compare for a equal match or not on both strings letters and sequence
+		// and return boolean value true or false
 		return a.equals(b);
-    }
-	
-	//created a function to sort string into alphabetical order
+	}
+
+	// created a function to sort string into alphabetical order
 	public static String alphabSortString(String inputString) {
-		
-		//apply Apply toCharArray() method on input string to create a char array for input string.
+
+		// apply Apply toCharArray() method on input string to create a char array for
+		// input string.
 		char[] tempArray = inputString.toCharArray();
-		
-		//Use Arrays.sort(char c[]) method to sort char array.
+
+		// Use Arrays.sort(char c[]) method to sort char array.
 		Arrays.sort(tempArray);
-		
-		//Use String class constructor to create a sorted string from char array. Return alphabetical order string
-		return new String(tempArray);	
-		
+
+		// Use String class constructor to create a sorted string from char array.
+		// Return alphabetical order string
+		return new String(tempArray);
+
 	}
-	
+
 	public static void main(String[] args) {
-		    
-	        Scanner scan = new Scanner(System.in);
-	        String a = scan.next();
-	        String b = scan.next();
-	        scan.close();
-	        boolean ret = isAnagram(a, b);
-	        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
-	    }
+
+		Scanner scan = new Scanner(System.in);
+		String a = scan.next();
+		String b = scan.next();
+		scan.close();
+		boolean ret = isAnagram(a, b);
+		System.out.println((ret) ? "Anagrams" : "Not Anagrams");
 	}
+}
---
+++ HackerRank/src/string_Challenages/java_String_Tokens.java
@@ -0,0 +1,14 @@
+package string_Challenages;
+
+import java.io.*;
+import java.util.*;
+
+public class java_String_Tokens {
+	 public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        String s = scan.nextLine();
+	        // Write your code here.
+	        scan.close();
+	    }	
+
+}
