4e931e3cb13033a69f28db9bab98f81b3798c1fa
Charles Adeeko <cadeeko@googlemail.com>
2018-04-29 16:44:10 +0100

    test(java_String_Tokens): completed challenge.

--- HackerRank/src/string_Challenages/java_String_Tokens.java
+++ HackerRank/src/string_Challenages/java_String_Tokens.java
@@ -4,11 +4,37 @@ import java.io.*;
 import java.util.*;
 
 public class java_String_Tokens {
-	 public static void main(String[] args) {
-	        Scanner scan = new Scanner(System.in);
-	        String s = scan.nextLine();
-	        // Write your code here.
-	        scan.close();
-	    }	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		// required for test case 9 where input is empty string
+		if (!scan.hasNext()) {
+			System.out.println(0);
+			scan.close();
+			return;
+			
+		}
+
+		String s = scan.nextLine();
+		scan.close();
+
+		// Write your code here.
+		// create regular expressions to split string into tokens
+		String regex = "[ .,?!'@_]+";
+
+		// store regular expressions results into string array using trim to removed
+		// leading or
+		// trailing whitespaces
+		String[] splitStringArray = s.trim().split(regex);
+
+		// print out number of tokens using length of array.
+		System.out.println(splitStringArray.length);
+
+		// Using a for loop to iterate over string array to print out content of each
+		// index.
+		for (String printStrings : splitStringArray)
+			System.out.println(printStrings);
+
+	}
 
 }
