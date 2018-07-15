28db9558ad1aaa1016c979f3d60ac8536e3076b3
Charles Adeeko <cadeeko@googlemail.com>
2018-07-15 20:44:01 +0100

    feat(Agile-Java): Lesson 1 examples

---
+++ Agile-Java/Lesson1/src/student_test/Student.java
@@ -0,0 +1,13 @@
+package student_test;
+
+class Student {
+	private String name;
+	
+	Student (String name) {
+		this.name = name;
+	}
+	
+	String getName() {
+		return name;
+	}
+}
---
+++ Agile-Java/Lesson1/src/student_test/StudentTest.java
@@ -0,0 +1,16 @@
+package student_test;
+
+public class StudentTest extends junit.framework.TestCase {
+	public void testCreate() {
+		final String firstStudentName = "Joe Doe";
+		Student firststudent = new Student(firstStudentName);
+		assertEquals(firstStudentName, firststudent.getName());
+		
+		final String secondStudentName = "Joe Blow";
+		Student secondStudent = new Student(secondStudentName);
+		assertEquals(secondStudentName, secondStudent.getName());
+		
+	}
+
+}
+
